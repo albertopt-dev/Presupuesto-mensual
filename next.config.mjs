e569753b809e3b1cf68e85c65e64bb41382ba472
Alberto <alberto.pt.dev@gmail.com>
2026-04-12 10:55:43 +0200

    modificaion de archivo firebase

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import withPWA from "next-pwa";
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const defaultCache = require("next-pwa/cache");
-
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
@@ -16,24 +10,4 @@ const nextConfig = {
   },
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/firestore\.googleapis\.com\/.*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /^https:\/\/firebase\.googleapis\.com\/.*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /^https:\/\/identitytoolkit\.googleapis\.com\/.*/i,
-      handler: "NetworkOnly",
-    },
-    ...defaultCache,
-  ],
-})(nextConfig);
+export default nextConfig;
